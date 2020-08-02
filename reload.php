c8bee7044d60cf463dd723325e211d65e107d884
khairilamir96 <amirkhairil96@gmail.com>
2020-08-02 19:06:19 +0800

    Update reload.php

--- reload.php
+++ reload.php
@@ -1,19 +1,80 @@
 <?php include('config.php');
-session_start();
+if (!isset($_SESSION)) {
+  session_start();
+}
+$MM_authorizedUsers = "";
+$MM_donotCheckaccess = "true";
+
+// *** Restrict Access To Page: Grant or deny access to this page
+function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
+  // For security, start by assuming the visitor is NOT authorized. 
+  $isValid = False; 
+
+  // When a visitor has logged into this site, the Session variable MM_Username set equal to their username. 
+  // Therefore, we know that a user is NOT logged in if that Session variable is blank. 
+  if (!empty($UserName)) { 
+    // Besides being logged in, you may restrict access to only certain users based on an ID established when they login. 
+    // Parse the strings into arrays. 
+    $arrUsers = Explode(",", $strUsers); 
+    $arrGroups = Explode(",", $strGroups); 
+    if (in_array($UserName, $arrUsers)) { 
+      $isValid = true; 
+    } 
+    // Or, you may restrict access to only certain users based on their username. 
+    if (in_array($UserGroup, $arrGroups)) { 
+      $isValid = true; 
+    } 
+    if (($strUsers == "") && true) { 
+      $isValid = true; 
+    } 
+  } 
+  return $isValid; 
+}
+
+$MM_restrictGoTo = "index.php";
+if (!((isset($_SESSION['username'])) && (isAuthorized("",$MM_authorizedUsers, $_SESSION['username'], $_SESSION['MM_UserGroup'])))) {   
+  $MM_qsChar = "?";
+  $MM_referrer = $_SERVER['PHP_SELF'];
+  if (strpos($MM_restrictGoTo, "?")) $MM_qsChar = "&";
+  if (isset($_SERVER['QUERY_STRING']) && strlen($_SERVER['QUERY_STRING']) > 0) 
+  $MM_referrer .= "?" . $_SERVER['QUERY_STRING'];
+  $MM_restrictGoTo = $MM_restrictGoTo. $MM_qsChar . "accesscheck=" . urlencode($MM_referrer);
+  header("Location: ". $MM_restrictGoTo); 
+  exit;
+}
+
+$currentPage = $_SERVER["PHP_SELF"];
 
 $username = $_SESSION["username"];
-$password = $_SESSION["password"];
 $password = $_SESSION["password"];
 
 $query_admin = $mysqli->query("SELECT * FROM `admin` WHERE username='$username' AND password='$password'");
 $row_admin = $query_admin->fetch_assoc();
 $totalRows_admin = $query_admin->num_rows;
 
-if (isset($_POST['idd'])) 
-{
-  extract($_POST);
 
-  $query_admin2 = $mysqli->query("SELECT * FROM `admin` WHERE matric_id='$idd'");
+if (isset($_REQUEST["Amount"])) 
+{
+  $merchantcode = $_REQUEST["MerchantCode"];
+  $paymentid = $_REQUEST["PaymentId"];
+  $refno = $_REQUEST["RefNo"];
+  $amount = $_REQUEST["Amount"];
+  $ecurrency = $_REQUEST["Currency"];
+  $remark = $_REQUEST["Remark"];
+  $transid = $_REQUEST["TransId"];
+  $authcode = $_REQUEST["AuthCode"];
+  $estatus = $_REQUEST["Status"];
+  $errdesc = $_REQUEST["ErrDesc"];
+  $signature = $_REQUEST["Signature"];
+}
+else
+{
+  $estatus = 2;
+}
+
+if($estatus==1) 
+{
+  $query_admin2 = $mysqli->query("SELECT * FROM `admin` WHERE username='$username'");
   $row_admin2 = $query_admin2->fetch_assoc();
   $totalRows_admin2 = $query_admin2->num_rows;
 
@@ -40,8 +101,15 @@ if (isset($_POST['idd']))
   }
 
   
+$insertGoTo = "reload.php?notif=success";
+header(sprintf("Location: %s", $insertGoTo));
 
- 
+}
+elseif($estatus == 0)
+{
+
+$insertGoTo = "reload.php?notif=fail";
+header(sprintf("Location: %s", $insertGoTo));
 }
 
 
@@ -83,13 +151,32 @@ if (isset($_POST['idd']))
         <h4 class="uppercase">Car Manager</h4>
         <div class="col-sm-12">
           <br>
-          <form action="reload.php" method="POST">
+          <form method="POST" name="ePayment" action="https://www.mobile88.com/ePayment/entry.asp">
 
-            <label for="uname"><b>Matric/Staff ID</b></label>
+            <label class="text-color" for="uname"><b>Matric/Staff ID</b></label>
             <input type="text" placeholder="Enter Matric/Staff ID" name="idd" required value="">
 
-            <label for="uname"><b>Reload Amount (RM)</b></label>
-            <input type="text" placeholder="Enter Reload Amount" name="amount" required value="">
+            <label class="text-color" for="uname"><b>Reload Amount (RM)</b></label>
+            <input type="text" placeholder="Enter Reload Amount" name="Amount" required value="">
+
+            <label class="text-color" for="uname"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required value="">
+
+            <label class="text-color" for="uname"><b>Contact Number</b></label>
+            <input type="text" placeholder="Enter Contact Number" name="contact" required value="">
+
+            <label class="text-color" for="uname"><b>Remark</b></label>
+            <input type="text" placeholder="Enter Remark" name="remark" required value="">
+
+            <INPUT type="hidden" name="MerchantCode" value="M00003">
+            <INPUT type="hidden" name="PaymentId" value="">
+            <INPUT type="hidden" name="RefNo" value="A00000001">
+            <INPUT type="hidden" name="Currency" value="MYR">
+            <INPUT type="hidden" name="ProdDesc" value="Photo Print">
+            <INPUT type="hidden" name="Lang" value="UTF-8">
+            <INPUT type="hidden" name="Signature" value="84dNMbfgjLMS42IqSTPqQ99cUGA=">
+            <INPUT type="hidden" name="ResponseURL" value="http://localhost/jomsewa/reload.php">
+            <INPUT type="hidden" name="BackendURL" value="http://www.YourBackendURL.com/payment/backend_response.asp">
 
             <input type="hidden" name="admin_id" value="<?=$row_admin['admin_id']?>">
             <div style="margin: auto; display: inline-block; text-align: center; width: 100%;">
